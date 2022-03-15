c99da6548702a319f6a0ba253f4ef24c2962e874
missamarie1 <97714453+missamarie1@users.noreply.github.com>
2022-03-14 23:41:47 -0400

    unit1-js-practice

--- script.js
+++ script.js
@@ -27,3 +27,48 @@ console.log(`1) ${travelOptions[0]}`);
 console.log(`2) ${travelOptions[1]}`);
 console.log(`3) ${travelOptions[2]}`);
 console.log(`4) ${travelOptions[3]}`);
+
+let travelType = prompt("How would you like to travel?");
+console.log("Travel Type: " + travelType);
+
+let distance = 100;
+let time = 0;
+let cost = 0; 
+
+if (travelType === "foot") {
+    console.log("Walking is free! Speed is 3mph.");
+    cost = 0; 
+    time = distance / 3;
+}
+else if (travelType === "bike") {
+    let rentBike = prompt("Do you need to rent the bike? (yes or no)");
+    if (rentBike === "yes") {
+        console.log("Bike rental is $45! Speed is 10mph");
+        cost = 45;
+    }
+    else {
+    console.log("Biking is free! Speed is 10mph.");
+    cost = 0; 
+    }
+    time = distance / 10;
+
+}
+
+else if (travelType ==="car") {
+    console.log("Driving is $0.25/mi. Speed is 60mph.");
+    cost = .25 * distance;
+    time = distance / 60;
+}
+else if (travelType === "airplane") {
+    let passengerCount = prompt("How many passengers?");
+    console.log("Flying is $10/mi. Speed is 400 mph.");
+    cost = .10 * distance * passengerCount;
+    time = distance / 400;
+}
+else {
+    console.log(`Sorry. ${travelType} is an invalid option.`);
+
+}
+
+console.log(`Traveling ${distance} miles by ${travelType} took ${time} hours and cost $${cost}!`);
+
